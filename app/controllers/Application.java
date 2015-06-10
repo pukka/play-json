994fea843f286450ad735d5927b84a6d8b609d8e
FukazawaShun <breakdown547@gmail.com>
2015-06-10 14:16:48 +0900

    Add how to take json in view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,14 +9,14 @@ import play.data.Form;
 import models.*;
 
 public class Application extends Controller {
-
+    
     static Form<Forms.TaskForm> taskForm = Form.form(Forms.TaskForm.class);
 
     public Result index() {
         return ok(index.render(Task.all(),taskForm));
     }
 
-    public Result addTask(){
+    public Result addTask() {
 	Form<Forms.TaskForm>filledForm = taskForm.bindFromRequest();
         Task.create(filledForm.get());
 
